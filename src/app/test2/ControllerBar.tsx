2fc9813a0f425a1bd67476b29f4a6fdf4e7f5a46
Okhan97 <26289334+Okhan97@users.noreply.github.com>
2025-03-16 18:34:02 -0300

    Added controls

---
+++ src/app/test2/ControllerBar.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { Button, Checkbox, TextField } from "@mui/material";
+import { Dispatch, SetStateAction, useState } from "react";
+import { ChromePicker } from "react-color";
+
+const DEFAULT_INTERVAL_MS = 1000;
+const DEFAULT_CIRCLE_RADIUS = 1;
+
+type ControllerBarProps = {
+  intervalMs: {
+    get: number;
+    set: Dispatch<SetStateAction<number>>;
+  };
+  circleRadius: {
+    get: number;
+    set: Dispatch<SetStateAction<number>>;
+  };
+  color: {
+    get: string;
+    set: Dispatch<SetStateAction<string>>;
+  };
+  iterateRandom: {
+    get: boolean;
+    set: Dispatch<SetStateAction<boolean>>;
+  };
+};
+
+const ControllerBar = ({
+  intervalMs,
+  circleRadius,
+  color,
+  iterateRandom,
+}: ControllerBarProps) => {
+  const [openColorPicker, setOpenColorPicker] = useState(false);
+
+  return (
+    <div className="flex gap-4 px-8 py-4 relative z-10 bg-neutral-600">
+      <div className="flex items-center">
+        <Checkbox
+          title="wena"
+          value={iterateRandom.get}
+          onChange={() => iterateRandom.set(!iterateRandom.get)}
+        />
+        <div>Iterate random color</div>
+      </div>
+      <TextField
+        disabled={!iterateRandom.get}
+        label="Interval (ms)"
+        type="number"
+        value={isNaN(intervalMs.get) ? "" : intervalMs.get}
+        onChange={(e) => {
+          if (e.target.value === "") {
+            intervalMs.set(NaN);
+            return;
+          }
+          const newValue = parseInt(e.target.value);
+          if (!isNaN(newValue)) intervalMs.set(newValue);
+        }}
+        onBlur={() => {
+          if (isNaN(intervalMs.get)) intervalMs.set(DEFAULT_INTERVAL_MS);
+        }}
+      />
+      <TextField
+        label="Circle Radius (px)"
+        type="number"
+        value={isNaN(circleRadius.get) ? "" : circleRadius.get}
+        onChange={(e) => {
+          if (e.target.value === "") {
+            circleRadius.set(NaN);
+            return;
+          }
+          const newValue = parseInt(e.target.value);
+          if (!isNaN(newValue)) circleRadius.set(newValue);
+        }}
+        onBlur={() => {
+          if (isNaN(circleRadius.get)) circleRadius.set(DEFAULT_CIRCLE_RADIUS);
+        }}
+      />
+      <div className="relative">
+        <Button onClick={() => setOpenColorPicker(!openColorPicker)}>
+          Pick color
+        </Button>
+        {openColorPicker && (
+          <ChromePicker
+            className="absolute"
+            color={color.get}
+            onChange={(e) => color.set(e.hex)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ControllerBar;
--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -3,27 +3,33 @@
 import { Circle } from "@/types";
 import { getRandomColor, getSimilarRandomColor } from "@/utils/colors";
 import { randInt } from "@/utils/numbers";
-import { Checkbox } from "@mui/material";
 import { RefObject, useEffect, useRef, useState } from "react";
 import { drawCircles } from "./drawCircles";
+import ControllerBar from "./ControllerBar";
 
 const MAX_CIRCLES = 2 ** 17;
-const COLOR_VARIATION = 1000;
-const CIRCLE_RADIUS = 1;
-const INTERVAL_MS = 2000;
+const COLOR_VARIATION = 100;
 
 const CanvasCircle = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [intervalMs, setIntervalMs] = useState(1000);
+  const [circleRadius, setCircleRadius] = useState(1);
+  const [color, setColor] = useState(getRandomColor());
+  const [iterateRandom, setIterateRandom] = useState(false);
 
   //TODO: This 3 should be one state
   const [showRed, setShowRed] = useState(true);
   const [showGreen, setShowGreen] = useState(true);
   const [showBlue, setShowBlue] = useState(true);
 
-  const [color, setColor] = useState(getRandomColor());
-  const [nCircles, setNCircles] = useState(1);
+  const [nCircles, setNCircles] = useState(2 ** 7);
   const circlesRef = useRef<Circle[]>([]);
   const blueCirclesRef = useRef<Circle[]>([]);
+
+  // Set starting circles
+  useEffect(() => {
+    addNewCircles(nCircles, circlesRef, circleRadius);
+  }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -31,24 +37,23 @@ const CanvasCircle = () => {
     if (typeof window === "undefined") return;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    // drawCircles({
-    //   canvas,
-    //   circlesRef,
-    //   color,
-    // });
     drawCircles({
       canvas,
       circlesRefList: [circlesRef, blueCirclesRef],
-      color: getRandomColor(),
+      color,
     });
   }, [color, nCircles, showBlue]);
 
-  const addNewCircles = (prev: number, ref: RefObject<Circle[]>): number => {
+  const addNewCircles = (
+    prev: number,
+    ref: RefObject<Circle[]>,
+    circleRadius: number
+  ): number => {
     const newCount = Math.min(prev * 2, MAX_CIRCLES);
     const newCircles = Array.from({ length: newCount - prev }, () => ({
       x: Math.random() * window.innerWidth,
       y: Math.random() * window.innerHeight,
-      r: CIRCLE_RADIUS + randInt(1, 10),
+      r: circleRadius + randInt(1, 10),
     }));
     ref.current = [...ref.current, ...newCircles];
     return newCount;
@@ -66,41 +71,43 @@ const CanvasCircle = () => {
           return newCount;
         });
       }
-      if (e.code === "KeyS") {
-        setNCircles((prev) => {
-          const newCount = Math.max(1, Math.floor(prev / 2));
-          blueCirclesRef.current = blueCirclesRef.current.slice(0, newCount);
-          return newCount;
-        });
-      }
       if (e.code === "ArrowUp")
-        setNCircles((prev) => addNewCircles(prev, circlesRef));
-      if (e.code === "KeyW")
-        setNCircles((prev) => addNewCircles(prev, blueCirclesRef));
+        setNCircles((prev) => addNewCircles(prev, circlesRef, circleRadius));
     };
 
     window.addEventListener("keydown", handleKeyPress);
     return () => window.removeEventListener("keydown", handleKeyPress);
-  }, []);
+  }, [circleRadius, color]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setColor(getSimilarRandomColor(color, COLOR_VARIATION));
-    }, INTERVAL_MS);
+      if (iterateRandom)
+        setColor(getSimilarRandomColor(color, COLOR_VARIATION));
+    }, intervalMs);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [color, intervalMs, iterateRandom]);
 
   return (
-    <div className="">
-      <div className="border-red-500 border px-6 py-2 relative z-10">
-        <Checkbox checked={showRed} onChange={() => setShowRed(!showRed)} />
-        <Checkbox
-          checked={showGreen}
-          onChange={() => setShowGreen(!showGreen)}
-        />
-        <Checkbox checked={showBlue} onChange={() => setShowBlue(!showBlue)} />
-      </div>
+    <div>
+      <ControllerBar
+        intervalMs={{
+          get: intervalMs,
+          set: setIntervalMs,
+        }}
+        circleRadius={{
+          get: circleRadius,
+          set: setCircleRadius,
+        }}
+        color={{
+          get: color,
+          set: setColor,
+        }}
+        iterateRandom={{
+          get: iterateRandom,
+          set: setIterateRandom,
+        }}
+      />
       <canvas
         ref={canvasRef}
         className="absolute top-0 left-0 w-full h-full z-9"
